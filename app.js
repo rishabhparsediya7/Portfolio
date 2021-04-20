24e68a47d5ef72a2d1f2fcaf423ee2dc3d54b31e
Rishabh Parsediya <parsediyarishabh@gmail.com>
2021-04-20 23:33:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// var modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// function modal() {
+//     modal.style.display = "block";
+
+// }
+
+// function change(src) {
+//     modal();
+//     document.getElementById('main').src = src;
+// }
+// // const change = src => {
+// //     document.getElementById('main').src = src
+// // }
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
